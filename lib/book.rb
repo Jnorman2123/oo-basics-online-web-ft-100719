92db3f3d36e454e4ee774934e3b832d9f6cbc887
Jonathan Norman <jnorman2123@gmail.com>
2019-10-13 20:35:41 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,24 @@
 class Book
-  def initialize(title)
+  def initialize(title, author_name, page_count, genre)
     @title = title
+    @author_name = author_name
+    @page_count = page_count
+    @genre = genre
   end
 
   def title
     @title
+  end
+
+  def author_name
+    @author_name
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre
+    @genre
   end
 end
